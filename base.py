294c98cdc38e1c10b1e77fcad6ec086969801421
ESarah77 <153368896+ESarah77@users.noreply.github.com>
2024-03-28 16:00:00 +0100

    Module, Loss, MSELoss

---
+++ base.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+class Loss(object):
+    def forward(self, y, yhat):
+        pass
+
+    def backward(self, y, yhat):
+        pass
+
+class Linear(Module):
+    def __init__(self, input, output):
+        self.input = input
+        self.output = output
+    
+    def forward(self, X):
+        return X@self._parameters
+
+class MSELoss(Loss):
+    def forward(self, y, yhat):
+        # y et yhat de taille batch x d
+        return ((y - yhat)**2).mean(axis=1)        
+
+class Module(object):
+    def __init__(self):
+        self._parameters = None
+        self._gradient = None
+
+    def zero_grad(self):
+        ## Annule gradient
+        pass
+
+    def forward(self, X):
+        ## Calcule la passe forward
+        pass
+
+    def update_parameters(self, gradient_step=1e-3):
+        ## Calcule la mise a jour des parametres selon le gradient calcule et le pas de gradient_step
+        self._parameters -= gradient_step*self._gradient
+
+    def backward_update_gradient(self, input, delta):
+        ## Met a jour la valeur du gradient
+        pass
+
+    def backward_delta(self, input, delta):
+        ## Calcul la derivee de l'erreur
+        pass
