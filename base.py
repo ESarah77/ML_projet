299ae42827be69617a1cfcf2e1a3c1099079490c
ESarah77 <153368896+ESarah77@users.noreply.github.com>
2024-05-20 18:49:27 +0200

    Jusqu'à la 4e partie (mais résultats étranges...)

--- base.py
+++ base.py
@@ -1,6 +1,31 @@
 import numpy as np
 import torch
 
+########################### FONCTIONS UTILES ###################################
+
+def prevent_overflow(data): # Fonction à utiliser dès qu'une valeur est donnée à l'exponentielle
+    # 709 = borne supérieure pour éviter l'overflow lorsqu'un très grand nombre est donné à l'exponentielle
+    data_without_overflow = np.where(data > 709, 709, data)
+    data_without_overflow = np.where(data_without_overflow < -709, -709, data_without_overflow)
+    return data_without_overflow
+
+def one_hot_encode(labels, num_classes):
+    one_hot = np.zeros((labels.size, num_classes))
+    one_hot[np.arange(labels.size), labels] = 1
+    return one_hot
+
+def accuracy_multiclasse(predictions, targets):
+    predicted_classes = np.argmax(predictions, axis=1)
+    true_classes = np.argmax(targets, axis=1)
+    accuracy = np.mean(predicted_classes == true_classes)
+    return accuracy
+
+def accuracy_bin(predictions, targets):
+    pred = np.where(predictions > 0.5, 1, 0)
+    return np.mean(pred == targets)
+
+################################# CLASSES DE BASES ################################
+
 class Loss(object):
     def forward(self, y, yhat):
         pass
@@ -110,10 +135,7 @@ class Sigmoide(Module):
         pass
 
     def forward(self, data):
-        # 709 = borne supérieure pour éviter l'overflow lorsqu'un très grand nombre est donné à l'exponentielle
-        data_without_overflow = np.where(data > 709, 709, data)
-        data_without_overflow = np.where(data_without_overflow < -709, -709, data_without_overflow)
-
+        data_without_overflow = prevent_overflow(data)
         return 1 / (1 + np.exp(-data_without_overflow))
         
     def update_parameters(self, gradient_step=1e-3):
@@ -193,13 +215,19 @@ class Optim(object):
 
         self._net.update_parameters(self._eps)
 
-        return loss.mean()
+        if yhat.shape[-1] == 1: # classes binaires
+            accuracy = accuracy_bin(yhat, batch_y)
+        else:
+            accuracy = accuracy_multiclasse(yhat, batch_y)
+
+        return loss.mean(), accuracy
     
-def SGD(net, loss, X, Y, batch_size, n_iter, learning_rate, milestone):
+def SGD(net, loss, X, Y, batch_size, n_iter, learning_rate):
     # Découpage en batch et apprentissage du réseau
     opt = Optim(net, loss, learning_rate)
     nb_samples = X.shape[0]
     loss_history = []
+    accuracy_history = []
 
     for i in range(n_iter):
         # Shuffle the examples
@@ -208,6 +236,7 @@ def SGD(net, loss, X, Y, batch_size, n_iter, learning_rate, milestone):
         data_shuffled = X[indices]
         labels_shuffled = Y[indices]
         tmp_loss = [] # loss for each batch
+        tmp_acc = []
 
         # Mini-batch training
         for index_start in range(0, nb_samples, batch_size):
@@ -215,11 +244,92 @@ def SGD(net, loss, X, Y, batch_size, n_iter, learning_rate, milestone):
             batch_x = data_shuffled[index_start:index_end]
             batch_y = labels_shuffled[index_start:index_end]
 
-            loss_value = opt.step(batch_x, batch_y)
+            loss_value, accuracy_value = opt.step(batch_x, batch_y)
             tmp_loss.append(loss_value)
+            tmp_acc.append(accuracy_value)
+        print(tmp_acc)
         
-        # Keep a record of the loss every milestone epochs
-        if i % milestone == 0:
-            loss_history.append(np.mean(tmp_loss)) # mean of the loss of each batch
+        loss_history.append(np.mean(tmp_loss)) # mean of the loss of each batch
+        accuracy_history.append(np.mean(tmp_acc))
 
-    return loss_history
+    return loss_history, accuracy_history
+
+######################### 4E PARTIE : Softmax, LogSoftmax, CELogSoftmax ###############################
+
+class Softmax(Module):
+    def __init__(self):
+        super().__init__()
+
+    def zero_grad(self):
+        ## Annule gradient / Réinitialiser à 0 le gradient
+        pass
+
+    def forward(self, X):
+        ## Calcule la passe forward
+        data_without_overflow = X - np.max(X, axis=1, keepdims=True)
+        exp_data = np.exp(data_without_overflow) # soustraire par le max pour éviter les instabilités numériques
+        return exp_data / np.sum(exp_data, axis=1, keepdims=True) # probas normalisées
+
+    def update_parameters(self, gradient_step=1e-3):
+        ## Calcule la mise a jour des parametres selon le gradient calcule et le pas de gradient_step
+        pass
+
+    def backward_update_gradient(self, input, delta):
+        ## Met a jour la valeur du gradient
+        pass
+
+    def backward_delta(self, input, delta):
+        ## Calcul la derivee de l'erreur
+        output = self.forward(input)
+        return delta * output * (1 - output)
+
+
+class LogSoftmax(Module):
+    def __init__(self):
+        super().__init__()
+
+    def zero_grad(self):
+        ## Annule gradient / Réinitialiser à 0 le gradient
+        pass
+
+    def forward(self, X):
+        ## Calcule la passe forward
+        max_data = np.max(X, axis=1, keepdims=True)
+        data_without_overflow = prevent_overflow(X - max_data)
+        log_sum_exp = np.log(np.sum(np.exp(data_without_overflow), axis=1, keepdims=True))
+        return X - max_data - log_sum_exp
+
+    def update_parameters(self, gradient_step=1e-3):
+        ## Calcule la mise a jour des parametres selon le gradient calcule et le pas de gradient_step
+        pass
+
+    def backward_update_gradient(self, input, delta):
+        ## Met a jour la valeur du gradient
+        pass
+
+    def backward_delta(self, input, delta):
+        ## Calcul la derivee de l'erreur
+        output = self.forward(input)
+        data_without_overflow = prevent_overflow(output)
+        e = np.exp(data_without_overflow)
+        return delta * (1 - e / np.sum(e, axis=1, keepdims=True))
+
+
+class CE(Loss):
+    def forward(self, y, yhat): # y : one-hot encoded
+        return -np.sum(y * yhat, axis=1) # y et yhat matrix
+
+    def backward(self, y, yhat):
+        return yhat - y
+
+
+class CELogSoftmax(Loss):
+    def forward(self, y, yhat):
+        data_without_overflow = prevent_overflow(yhat)
+        return -np.sum(y * yhat, axis=1) + np.log(np.sum(np.exp(data_without_overflow), axis=1)) 
+
+    def backward(self, y, yhat):
+        data_without_overflow = prevent_overflow(yhat)
+        e = np.exp(data_without_overflow)
+        return -y + e / (np.sum(e, axis=1).reshape((-1, 1)))
+    
